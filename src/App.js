0f9f5a3bd2154056c3ee98768cc576611fca9720
OhmPatil <workpatil10@gmail.com>
2022-10-02 00:39:27 +0530

    Clean boilerplate and add google auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { collection, addDoc, onSnapshot, doc } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { database, auth, signInWithGoogle, logout } from "./firebase";
+
 import './App.css';
 
 function App() {
+
+  const [user] = useAuthState(auth)
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      await addDoc(collection(database, "people"), {
+        name: name,
+        email: email,
+      });
+    } catch (error) {
+      alert(error);
+    }
+
+    setEmail("");
+    setName("");
+    console.log('clicked');
+  }
+
+  useEffect(() => {
+    if(user) console.log(user);
+  }, [user])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {user && <pre>Logged in as {user.displayName}</pre>}
+    {user && <button onClick={logout}>Sign out</button>}
+    <input type='text' value={name} onChange={(event) => setName(event.target.value)}/>
+    <input type='text' value={email} onChange={(event) => setEmail(event.target.value)}/>
+    <button type='submit' onClick={(event) => handleSubmit(event)}>Submit</button>
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </>
+  )
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,97 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, query, getDocs, collection, where, addDoc } from "firebase/firestore";
+import {GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword, sendPasswordResetEmail, signOut, createUserWithEmailAndPassword} from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDdQAXbJBJ50d47Hh8LDhBGUbAHwb2VVpg",
+    authDomain: "neofest-e1055.firebaseapp.com",
+    projectId: "neofest-e1055",
+    storageBucket: "neofest-e1055.appspot.com",
+    messagingSenderId: "1080635997977",
+    appId: "1:1080635997977:web:a91b11980a73dcfcc0cf88",
+    measurementId: "G-9MGYCH5ST8"
+  };
+  
+
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider();
+
+const signInWithGoogle = async (event) => {
+  event.preventDefault()
+  try {
+    const response = await signInWithPopup(auth, googleProvider)
+    const user = response.user;
+    const q = query(collection(database, 'users'), where('uid', '==', user.uid))
+    const docs = await getDocs(q)
+    if (docs.docs.length === 0) {
+      await addDoc(collection(database, 'users'), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: 'google',
+        email: user.email
+      })
+    }
+  }
+  catch(error) {
+    alert(error.message);
+  }
+}
+
+// const logInWithEmailAndPassword = async (event, email, password) => {
+//   event.preventDefault();
+//   try {
+//     await signInWithEmailAndPassword(auth, email, password)
+//   }
+//   catch(error){
+//     alert(error.message)
+//     window.location.reload()
+//   }
+// }
+
+// const registerUserWithEmailAndPassword = async (event, name, email, password) => {
+//   event.preventDefault()
+//   if(!name) {
+//     alert("Name cannot be blank <3")
+//     return
+//   }
+//   try {
+//     const response = await createUserWithEmailAndPassword(auth, email, password)
+//     const user = response.user
+//     await addDoc(collection(database, 'users'), {
+//       uid: user.uid,
+//       displayName: name,
+//       authProvider: 'local',
+//       email: email,
+//     })
+//   }
+//   catch(error){
+//     alert(error.message)
+//   }
+// }
+
+// const sendPasswordReset = async (email) => {
+//   try{
+//     await sendPasswordResetEmail(auth, email)
+//     alert('Reset Link Sent <3')
+//   }
+//   catch(error) {
+//     alert(error.message)
+//   }
+// }
+
+const logout = () => {
+  signOut(auth);
+}
+
+export { 
+  database,
+  auth,
+  signInWithGoogle, 
+//   logInWithEmailAndPassword, 
+//   registerUserWithEmailAndPassword,
+//   sendPasswordReset, 
+  logout 
+};
+
