25f14ffbbc499cfd12fcc7e38a59e930f00b2d57
OhmPatil <workpatil10@gmail.com>
2022-10-03 20:26:20 +0530

    Temporary mobile responsive fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,9 +24,9 @@ function Hero(props) {
 
   return (
     <>
-      <div className={`background ${grayScale}`} id="hero">
+      <div className={`background ${grayScale} flex flex-col w-screen justify-center items-center`} id="hero">
         <h1
-          className="fixed bg-transparent text-transparent bg-clip-text font-bold text-[4rem] top-[25%] left-[12.5%] text-center mb-6 bg-gradient-to-r from-[#BFA8FF] via-[#FABAA4] to-[#FFF08C] md:invisible"
+          className="fixed bg-transparent text-transparent bg-clip-text font-bold text-[4rem] text-center top-[30%] bg-gradient-to-r from-[#BFA8FF] via-[#FABAA4] to-[#FFF08C] md:invisible"
           id="mobile-text"
         >
           NeoFest
